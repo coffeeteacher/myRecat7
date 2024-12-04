e964043097bf481b278a6261a33cff783e78020a
coffeeteacher <coffeeteacher@gmail.com>
2024-12-04 11:37:11 +0800

    1204-2

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { IoUmbrellaOutline } from "react-icons/io5";
 import './App.css'
 
 function App() {
-  const [weatherList, setWeatherList] = useState([]);
-  
+  const [weatherList, setWeatherList] = useState([]); 
+
   useEffect(() => {
     (async () => {
       // 本地端要在web-server伺服器下才可以執行
@@ -16,7 +16,7 @@ function App() {
       const data = await axios.get('https://coffeeteacher.github.io/myRecat7/json/F-C0032-001.json');
       // console.log(data);
       const { location } = data.data.cwaopendata.dataset;
-      // console.log(location);
+      console.log(location);
       setWeatherList(location);
     })()
   }, [])
@@ -28,6 +28,68 @@ function App() {
 
     <div className="wrap">
       <h2>36小時天氣預報</h2>
+      <div className="container">
+        {
+          weatherList.map((city) => {
+            return (
+              <div className="item" key={city.locationName}>
+                <h3>{city.locationName}</h3>
+                <div className="content">
+                  {
+                    city.weatherElement[0].time.map((time, index) => {
+                      return (
+                        <div className="item2" key={index}>
+                          {/* 日期 */}
+                          <p>
+                            {
+                              new Date(time.startTime).toLocaleString(undefined, {
+                                day: 'numeric'
+                              })
+                            }
+                          </p>
+                          {/* 時間 */}
+                          <p>
+                            {
+                              new Date(time.startTime).toLocaleString(undefined, {
+                                hour: 'numeric',
+                                minute: 'numeric'
+                              })
+                            }
+                            <br />~<br />
+                            {
+                              new Date(time.endTime).toLocaleString(undefined, {
+                                hour: 'numeric',
+                                minute: 'numeric'
+                              })
+                            }
+                          </p>
+                          
+                          {/* 天氣圖 */}
+                          <p><img src={`./weatherIcon/${time.parameter.parameterName}.svg`} alt="" /></p>
+                          
+                          {/* 天氣圖名稱 */}
+                          <p>{time.parameter.parameterName}</p>
+
+                          {/* 降雨率 */}
+                          {/* <p><IoUmbrellaOutline />30%</p> */}
+                          <p>
+                            <IoUmbrellaOutline />
+                            {
+                              city.weatherElement[4].time[index].parameter.parameterName
+                            }
+                          </p>
+                        </div>
+                      )
+
+                    })
+                  }
+                </div>
+              </div>
+            )
+          })
+        }
+
+      </div>
     </div>
 
   )
