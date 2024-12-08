41b3c867b4ffc8442b9cadadb1df6a776ac7b11e
咖啡老師 <coffeeteacher@gmail.com>
2024-12-09 00:50:33 +0800

    jquery-1

--- src/pages/Jquery.jsx
+++ src/pages/Jquery.jsx
@@ -2,17 +2,31 @@ import { useEffect } from 'react'
 import $ from 'jquery'
 
 export default function Jquery() {
+
     useEffect(() => {
         $('#btn1').on('click', function () {
             // console.log('hi')
             alert($('#btn1').text());
         })
+
+        $('p').on('mouseover', function () {
+            $('p').animate({ fontSize: '22px' }, 300)
+        }).on('mouseout', function () {
+            $('p').animate({ fontSize: '16px' }, 300)
+        })
     }, [])
 
     return (
         <div>
-            <h1>jQuery</h1>
-            <button id='btn1'>Click</button>            
+            <h1 onMouseEnter={() => {
+                console.log($('h1').text())
+            }}>jQuery</h1>
+
+            <button id='btn1' onClick={() => {
+                console.log($('#btn1').text())
+            }}>Click</button>
+
+            <p>onMouseEnter</p>
         </div>
     )
 }
