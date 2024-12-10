4166bf6667516f4dfaf933aedb6417cd80905dc8
coffeeteacher <coffeeteacher@gmail.com>
2024-12-10 14:38:04 +0800

    1210-jquery-3

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
     return (
-        <div>
+        <div id="nav">
             <ul>
                 <li><Link to='/'>首頁</Link></li>
                 <li><Link to='/about'>關於我</Link></li>
