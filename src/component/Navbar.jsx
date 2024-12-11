576712f25152d4d20a509dd394292edd19ea65e9
coffeeteacher <coffeeteacher@gmail.com>
2024-12-11 11:44:31 +0800

    1211-react

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -8,6 +8,8 @@ export default function Navbar() {
                 <li><Link to='/about'>關於我</Link></li>
                 <li><Link to='/news'>最新消息</Link></li>
                 <li><Link to='/jquery'>jQuery</Link></li>                  
+                <li><a href="./html/jquery2.html" target="_blank">html新分頁</a></li> 
+                <li><a href="./html/jquery1.html">html不分頁</a></li> 
             </ul>
         </div>
     )
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,3 +1,10 @@
+import { Link } from "react-router-dom";
+
 export default function About() {
-    return <div>我是About頁</div>
+    return (
+        <div>
+            我是About頁
+            <Link to='/news'>最新消息</Link>
+        </div>
+    )
 }
