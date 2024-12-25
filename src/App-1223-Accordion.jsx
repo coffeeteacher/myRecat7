7aff7ab44a901e984b2b4d0efc577c071e749bd7
coffeeteacher <coffeeteacher@gmail.com>
2024-12-25 15:21:18 +0800

    1225-localstorage

--- src/App-1223-Accordion.jsx
+++ src/App-1223-Accordion.jsx
@@ -1,110 +1,109 @@
-// 折疊QA => Accordion
-// https://motion.dev/docs/react-quick-start
-// npm install motion
-
-import { useState } from "react"
-import { motion, AnimatePresence } from "motion/react";
+import { useState } from "react";
+import { AnimatePresence, motion } from "motion/react"
 import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
 
-
 export default function App() {
+  const [activeQuestionId, setActiveQuestionId] = useState(null);
+  // 折疊資料
+  const questions = [
+    {
+      id: 1,
+      question: "題目1",
+      answer: "答案1",
+    },
+    {
+      id: 2,
+      question: "題目2",
+      answer: "答案2",
+    },
+    {
+      id: 3,
+      question: "題目3",
+      answer: "答案3",
+    },
+  ];
 
-    // 作用中的變數
-    const [activeQuestion, setActiveQuestion] = useState(null);
-
-    // FAQ資料
-    const questions = [
-        {
-            id: 1,
-            question: "題目1",
-            answer: "答案1",
-        },
-        {
-            id: 2,
-            question: "題目2",
-            answer: "答案2",
-        },
-        {
-            id: 3,
-            question: "題目3",
-            answer: "答案3",
-        },
-    ];
-
-    return (
-        // 最外層
-        <div style={{
-            maxWidth: "100vw",
-            height: "100vh",
-            backgroundColor: "black",
-            display: "flex",
-            justifyContent: "center",
-            alignItems: "center",
-            // border:"1px solid red",
+  return (
+    <>
+      {/* 最外層 */}
+      <div className="wrapper" style={{
+        backgroundColor: "black",
+        maxWidth: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        color: "#ccc",
+      }}>
+        {/* FAQ區 */}
+        <div className="faq" style={{
+          backgroundColor: "gray",
+          width: "80%",
+          padding: "8px",
+          borderRadius: "5px",
         }}>
-            {/* Accordion面板 */}
-            <div style={{
-                width: "90%",
-                margin: "auto",
-                maxWidth: "1400px",
-                backgroundColor: "gray",
-                padding: "8px",
-                borderRadius: "5px",
-            }}>
-                {/* FAQ標題 */}
-                <h2 style={{
-                    textAlign: "center",
-                    marginBottom: "6px",
-                }}>FAQ標題</h2>
-                {
-                    // 帶出陣列中的資料
-                    questions.map((q) => {
-                        return (
-                            <div key={q.id} style={{
-                                marginBottom: "4px",
-                            }}>
-                                {/* QA按鈕 */}
-                                <button
-                                    style={{
-                                        width: "100%",
-                                        textAlign: "left",
-                                        border: "none",
-                                        outline: "none",
-                                        padding: "4px",
-                                        borderRadius: "2px",
-                                        backgroundColor: "#ccc",
-                                        display: "flex",
-                                        justifyContent: "space-between",
-                                        justifyItems: "center",
-                                    }}
-                                    onClick={() => setActiveQuestion(activeQuestion === q.id ? null : q.id)}
-                                >
-                                    {/* 帶出題目 */}
-                                    {q.question}
-                                    {/* +, - icon的toggle */}
-                                    {activeQuestion === q.id ? <FaMinusCircle /> : <FaPlusCircle />}
-                                </button>
-                                {/* 折疊鈕動畫 */}
-                                <AnimatePresence>
-                                    {
-                                        activeQuestion === q.id && (
-                                            <motion.div
-                                                initial={{ opacity: 0, height: 0 }}
-                                                animate={{ opacity: 1, height: "auto" }}
-                                                exit={{ opacity: 0, height: 0 }}
-                                                style={{ marginTop: "5px", color: "#ccc" }}
-                                            >
-                                                {/* 答案 */}
-                                                {q.answer}
-                                            </motion.div>
-                                        )
-                                    }
-                                </AnimatePresence>
-                            </div>
-                        )
-                    })
-                }
-            </div>
+          {/* 主標題 */}
+          <h2 style={{
+            textAlign: "center",
+            marginBottom: "10px",
+          }}>FAQ列表</h2>
+          {
+            // 帶出陣列資料
+            questions.map((q) => {
+              return (
+                <div key={q.id} style={{ marginBottom: "5px" }}>
+                  {/* QA按鈕 */}
+                  <button
+                    style={{
+                      width: "100%",
+                      textAlign: "left",
+                      borderRadius: "2px",
+                      border: "none",
+                      outline: "none",
+                      padding: "10px",
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      fontSize: "20px",
+                    }}
+                    onClick={() => setActiveQuestionId(activeQuestionId === q.id ? null : q.id)}
+                  >
+                    {/* 帶出題目 */}
+                    {q.question}
+                    {/* 加減號icon */}
+                    {
+                      activeQuestionId === q.id ? <FaMinusCircle /> : <FaPlusCircle />
+                    }
+                  </button>
+
+                  {/* 折疊鈕動畫 motion => https://motion.dev/docs/react-quick-start */}
+                  <AnimatePresence>
+                    {
+                      // 作用中的id跟被展開的id相同時
+                      activeQuestionId === q.id && (
+                        <motion.div
+                          // 初始化
+                          initial={{ opacity: 0, height: 0 }}
+                          // 展開動畫
+                          animate={{ opacity: 1, height: "auto" }}
+                          // 折疊動畫
+                          exit={{ opacity: 0, height: 0 }}
+                          // 外觀樣式
+                          style={{ marginTop: "5px", color: "white", fontSize: "18px" }}
+                        >
+                          {/* 答案 */}
+                          {q.answer}
+                        </motion.div>
+                      )
+                    }
+                  </AnimatePresence>
+                </div>
+              )
+
+            })
+          }
         </div>
-    )
+      </div>
+    </>
+  )
 }
--- src/App.jsx
+++ src/App.jsx
@@ -1,109 +1,11 @@
-import { useState } from "react";
-import { AnimatePresence, motion } from "motion/react"
-import { FaMinusCircle, FaPlusCircle } from "react-icons/fa";
-
 export default function App() {
-  const [activeQuestionId, setActiveQuestionId] = useState(null);
-  // 折疊資料
-  const questions = [
-    {
-      id: 1,
-      question: "題目1",
-      answer: "答案1",
-    },
-    {
-      id: 2,
-      question: "題目2",
-      answer: "答案2",
-    },
-    {
-      id: 3,
-      question: "題目3",
-      answer: "答案3",
-    },
-  ];
-
   return (
     <>
-      {/* 最外層 */}
-      <div className="wrapper" style={{
-        backgroundColor: "black",
-        maxWidth: "100vw",
-        height: "100vh",
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        color: "#ccc",
-      }}>
-        {/* FAQ區 */}
-        <div className="faq" style={{
-          backgroundColor: "gray",
-          width: "80%",
-          padding: "8px",
-          borderRadius: "5px",
-        }}>
-          {/* 主標題 */}
-          <h2 style={{
-            textAlign: "center",
-            marginBottom: "10px",
-          }}>FAQ列表</h2>
-          {
-            // 帶出陣列資料
-            questions.map((q) => {
-              return (
-                <div key={q.id} style={{ marginBottom: "5px" }}>
-                  {/* QA按鈕 */}
-                  <button
-                    style={{
-                      width: "100%",
-                      textAlign: "left",
-                      borderRadius: "2px",
-                      border: "none",
-                      outline: "none",
-                      padding: "10px",
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      fontSize: "20px",
-                    }}
-                    onClick={() => setActiveQuestionId(activeQuestionId === q.id ? null : q.id)}
-                  >
-                    {/* 帶出題目 */}
-                    {q.question}
-                    {/* 加減號icon */}
-                    {
-                      activeQuestionId === q.id ? <FaMinusCircle /> : <FaPlusCircle />
-                    }
-                  </button>
-
-                  {/* 折疊鈕動畫 motion => https://motion.dev/docs/react-quick-start */}
-                  <AnimatePresence>
-                    {
-                      // 作用中的id跟被展開的id相同時
-                      activeQuestionId === q.id && (
-                        <motion.div
-                          // 初始化
-                          initial={{ opacity: 0, height: 0 }}
-                          // 展開動畫
-                          animate={{ opacity: 1, height: "auto" }}
-                          // 折疊動畫
-                          exit={{ opacity: 0, height: 0 }}
-                          // 外觀樣式
-                          style={{ marginTop: "5px", color: "white", fontSize: "18px" }}
-                        >
-                          {/* 答案 */}
-                          {q.answer}
-                        </motion.div>
-                      )
-                    }
-                  </AnimatePresence>
-                </div>
-              )
-
-            })
-          }
-        </div>
-      </div>
+      <h1>網頁儲存說明</h1>
+      <hr/>
+      {
+        console.log("我是js撰寫區")
+      }
     </>
   )
 }
