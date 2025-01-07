e9d81ce0adee4978006063f3589c591bbbd17e62
coffeeteacher <coffeeteacher@gmail.com>
2025-01-07 16:04:29 +0800

    firebase0107

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyDAAn82uiuQ2IbiocETC-K33hozc-vDGJ0",
-    authDomain: "fir-oauth-dcb79.firebaseapp.com",
-    projectId: "fir-oauth-dcb79",
-    storageBucket: "fir-oauth-dcb79.firebasestorage.app",
-    messagingSenderId: "253595175799",
-    appId: "1:253595175799:web:dd6451a90625cb44caef72"
+  apiKey: "AIzaSyDAAn82uiuQ2IbiocETC-K33hozc-vDGJ0",
+  authDomain: "fir-oauth-dcb79.firebaseapp.com",
+  projectId: "fir-oauth-dcb79",
+  storageBucket: "fir-oauth-dcb79.firebasestorage.app",
+  messagingSenderId: "253595175799",
+  appId: "1:253595175799:web:dd6451a90625cb44caef72"
 };
 
 // Initialize Firebase
